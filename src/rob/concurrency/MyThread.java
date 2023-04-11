5ed87e2c15e381c35227730373bed45282a90899
Rob Smith <robsmith965@gmail.com>
2023-04-11 15:19:47 -0700

    add concurrency example

---
+++ src/rob/concurrency/MyThread.java
@@ -0,0 +1,18 @@
+package rob.concurrency;
+
+public class MyThread extends Thread {
+    String name;
+    SynchronizedExample s;
+
+    public MyThread(String name, SynchronizedExample s)  {
+        this.name = name;
+        this.s = s;
+        start();
+    }
+
+	@Override
+    public void run()
+    {
+        s.printThread3Times(name);
+    }
+}
---
+++ src/rob/concurrency/SynchronizedExample.java
@@ -0,0 +1,30 @@
+package rob.concurrency;
+
+public class SynchronizedExample {
+	
+	/*
+	 * without synchronized, it's faster, and prints in unpredictable order.
+	 * with synchronized, it's slower, prints 111222.
+	 *
+	 * note that without the sleep, the first thread would complete
+	 * before the second even if the method was synchronized.
+	 */
+    public synchronized void printThread3Times(String name)
+    {
+        for (int i = 0; i < 3; i++) {
+        	System.out.println(name + " :: " + i);
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException
+    {
+    	SynchronizedExample s = new SynchronizedExample();
+        new MyThread("THREAD 1",s);
+        new MyThread("THREAD 2",s);
+    }
+}
